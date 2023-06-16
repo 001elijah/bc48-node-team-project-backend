db3c5fee5b2dad76963303a5df05a3b17b563eac
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-06-16 17:20:54 +0300

    add swagger + swagger.json template

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 require('dotenv').config()
 
 const authRouter = require('./routes/auth-routes')
@@ -12,6 +14,8 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/auth', authRouter)
 
