c1f9234e51ede79871f0bea60e5cd611d7ef16a3
GGalina <71042425+GGalina@users.noreply.github.com>
2023-06-18 00:48:06 +0400

    updated app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-app.use('/auth', authRouter)
+app.use('/user', authRouter)
 
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found' })
